fb1ce9e2d2d8d4bac53644f3879041dbfe00ab59
Benjamin Carlson <bjcarlson42@gmail.com>
2023-02-21 10:02:55 -0500

    BurnerPage x Nextjs example

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+      <script src="https://admin.burner.page/js/<your_team_hash_here>"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
